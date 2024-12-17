10776b88a30db6b6090ab21e29fb723bc7ae26ac
deslrey <2135232170@qq.com>
2024-12-17 19:05:52 +0800

    feat: 完成并使用antd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  srcDir: 'src/',
+  srcDir: 'src/',  // 确保项目结构正确
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  plugins: [
+    { src: '~/plugins/ant-design-vue.js', ssr: false }  // 不需要 src 目录前缀
+  ]
 })
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,14 @@
 <template>
-    <div>我是Index</div>
+    <div>
+        <a-button type="primary">Hello, Ant Design</a-button>
+        <a-input placeholder="请输入" />
+    </div>
 </template>
+
+<script setup>
+// 无需额外引入，Ant Design 会自动注册全局组件
+</script>
+
+<style scoped>
+/* 你可以在这里进行样式的定制 */
+</style>
