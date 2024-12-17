5fcad21f827ad17c92ac1b4d0e903a4ed2983d33
deslrey <2135232170@qq.com>
2024-12-17 20:07:02 +0800

    chore: 移除蚂蚁UI修改为饿了么UI并添加vue版本

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@ export default defineNuxtConfig({
   srcDir: 'src/',  // 确保项目结构正确
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  plugins: [
-    { src: '~/plugins/ant-design-vue.js', ssr: false }  // 不需要 src 目录前缀
-  ]
+  modules: ['@element-plus/nuxt'],
+  css: [
+    'element-plus/dist/index.css',
+    'element-plus/theme-chalk/display.css'
+  ],
 })
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,12 @@
 <template>
-    <div>
-        <a-button type="primary">Hello, Ant Design</a-button>
-        <a-input placeholder="请输入" />
-    </div>
-</template>
-
-<script setup>
-// 无需额外引入，Ant Design 会自动注册全局组件
-</script>
-
-<style scoped>
-/* 你可以在这里进行样式的定制 */
-</style>
+    <el-button @click="ElMessage('hello')">button</el-button>
+    <LazyElButton type="warning">lazy button</LazyElButton>
+  </template>
+  
+  <script lang="ts" setup>
+  import { ElMessage } from 'element-plus';
+  
+  
+  
+  </script>
+  
